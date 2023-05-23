5d5e3fa16d5159108fed24693b3a5efaa078cb3b
FranciscoCardita <franciscocardita@ua.pt>
2023-05-24 00:30:38 +0100

    Update wallet

--- frontend/lib/wallet.dart
+++ frontend/lib/wallet.dart
@@ -13,6 +13,7 @@ class Wallet extends StatefulWidget {
 
 class _WalletState extends State<Wallet> {
   int _selectedIndex = 0;
+  OverlayEntry? _overlayEntry;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -23,45 +24,90 @@ class _WalletState extends State<Wallet> {
       case 0:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Wallet())
+          MaterialPageRoute(builder: (context) => const Wallet())
         );
         break;
       case 1:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Tickets())
+          MaterialPageRoute(builder: (context) => const Tickets())
         );
         break;
       case 2:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Map())
+          MaterialPageRoute(builder: (context) => const Map())
         );
         break;
       case 3:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Profile())
+          MaterialPageRoute(builder: (context) => const Profile())
         );
         break;
       default:
     }
   }
 
+  void _showOverlay() {
+    OverlayState? overlayState = Overlay.of(context);
+    _overlayEntry = OverlayEntry(
+      builder: (BuildContext context) {
+        return Positioned(
+          top: MediaQuery.of(context).size.height * 0.4,
+          left: MediaQuery.of(context).size.width * 0.25,
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: Material(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Add Funds'),
+                      IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          _hideOverlay(); // Call the method to hide the overlay
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    overlayState?.insert(_overlayEntry!);
+  }
+
+  void _hideOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    _hideOverlay(); // Make sure to hide the overlay when disposing of the widget
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(28, 32, 37, 100),
-      // appBar: AppBar(
-      //     title: const Text('Social Summit'),
-      //     ),
+      extendBody: true,
+      backgroundColor: const Color.fromRGBO(28, 32, 37, 100),
       body: Center(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 42),
+            const SizedBox(height: 42),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
+              children: const [
                 SizedBox(width: 25,),
                 Text(
                   'Wallet',
@@ -71,13 +117,23 @@ class _WalletState extends State<Wallet> {
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.only(left: 200),
+              child: ElevatedButton(
+                onPressed: () {
+                  _showOverlay();
+                },
+                child: Text('Add Funds'),
+              ),
+            ),
           ],
         ),
       ),
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(50, 60, 68, 100),
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20.0),
             topRight: Radius.circular(20.0),
