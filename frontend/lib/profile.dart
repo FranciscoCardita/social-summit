f1c193cbd6dd1688650c8a16c9f5bc0e9baa3f5c
FranciscoCardita <franciscocardita@ua.pt>
2023-05-24 00:30:29 +0100

    Update profile

--- frontend/lib/profile.dart
+++ frontend/lib/profile.dart
@@ -23,25 +23,25 @@ class _ProfileState extends State<Profile> {
       case 0:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Wallet())
+          MaterialPageRoute(builder: (context) => const Wallet())
         );
         break;
       case 1:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Tickets())
+          MaterialPageRoute(builder: (context) => const Tickets())
         );
         break;
       case 2:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Map())
+          MaterialPageRoute(builder: (context) => const Map())
         );
         break;
       case 3:
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Profile())
+          MaterialPageRoute(builder: (context) => const Profile())
         );
         break;
       default:
@@ -51,16 +51,56 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(28, 32, 37, 100),
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
+      extendBody: true,
+      backgroundColor: const Color.fromRGBO(28, 32, 37, 100),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Profile',
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/croco.jpeg'),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'John Doe',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'john.doe@example.com',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de dados pessoais
+              },
+              child: const Text('Personal Data'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de notificações
+              },
+              child: const Text('Notifications'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de perguntas frequentes (FAQs)
+              },
+              child: const Text('FAQs'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Realizar logout
+              },
+              child: const Text('Sign Out'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              ),
             ),
           ],
         ),
